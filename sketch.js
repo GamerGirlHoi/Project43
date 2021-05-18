9f34817914890c2949895f5769f03d4b896fa168
GamerGirlHoi <78184163+GamerGirlHoi@users.noreply.github.com>
2021-05-18 17:05:25 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+var backImage,backgr;
+var player, player_running;
+var ground,ground_img, banana_Img;
+
+var END = 0;
+var PLAY = 1;
+var gameState = PLAY;
+
+var banana;
+
+function preload(){
+  backImage=loadImage("jungle.jpg");
+  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+  banana_Img = loadImage("banana.png");
+}
+
+function setup() {
+  createCanvas(800,400);
+  
+  backgr=createSprite(0,0,800,400);
+  backgr.addImage(backImage);
+  backgr.scale=1.5;
+  backgr.x=backgr.width/2;
+  backgr.velocityX=-4;
+  
+  player = createSprite(100,340,20,50);
+  player.addAnimation("Running",player_running);
+  player.scale = 0.1;
+  
+  ground = createSprite(400,350,800,10);
+  ground.x=ground.width/2;
+  ground.visible=false;
+  
+}
+
+function draw() { 
+  background(0);
+
+  if(gameState===PLAY){
+    
+    function spawnFood() {
+      x = random(100, 700);
+      y = random(100, 300);
+      banana = createSprite (x, y, 50, 50);
+      banana.addImage("banana", banana_Img);
+
+      banana.velocityX = -5;
+
+      if (banana.isTouching(player) || banana. x < 0) {
+        banana.lifetime = 0;
+      }
+    }
+    spawnFood(); 
+    if(backgr.x<100){
+    backgr.x=backgr.width/2;
+    }
+  
+    if(keyDown("space") ) {
+      player.velocityY = -12;
+    }
+    player.velocityY = player.velocityY + 0.8;
+  
+    player.collide(ground);
+
+  }
+
+  drawSprites();
+}
